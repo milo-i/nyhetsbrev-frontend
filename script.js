10ee0a500abdad7556382c6ea7ebd038d1e98ca2
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-04-19 17:03:30 +0200

    creates eventlistener for user to signup to newsletter and function to fetch post data to backend

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 let root = document.getElementById('root');
 
-
-let rootTemplate = `
-<nav class="wrapper">
- <a href="index.html"><img id="img" src="logo.png" alt="logo"></a>
-  <ul class="ul-list">
-   <li class="li-item"><a href="index.html">Hem</a></li>
-   <li class="li-item"><a href="#">Kontakta</a></li>
-   <li class="li-item"><a href="#">Om oss</a></li>
-  </ul>
- </nav>
-
+// Template for HTML
+let rootTemplate = `<nav class="wrapper">
+												<ul class="ul-list">
+												<li class="li-item"><a href="index.html">Hem</a></li>
+												<li class="li-item"><a href="#">Kontakta</a></li>
+												<li class="li-item"><a href="#">Om oss</a></li>
+											</ul>
+ 										</nav>
 
  <div class="user-input">
 
-  <label for="email">Skriv in din email</label>
+  <label for="email">Skriv in din email: </label>
   <input type="email" id="email" placeholder="Email">
 
-   <label for="password">Skriv in ditt lösenord</label>
-   <input type="password" id="password" placeholder="Lösenord">
+   <label for="password">Skriv in ditt lösenord: </label>
+   <input type="password" id="password" placeholder="Lösenord" required>
 
     <button class="signUpBtn" id="signUpBtn">Registrera dig</button>
     <button class="loginBtn" id="loginBtn">Logga in</button>
@@ -27,13 +24,57 @@ let rootTemplate = `
     <div id="wrapper">
      <label for="yes_no_radio">Vill du ta del av vårt nyhetsbrev?</label>
      <p>
-      <input type="radio" name="yes_no" value="yes" id="yes">Japp!!!</input>
+      <input type="radio" name="yes_no" value="true" id="yes">Japp!!!</input>
      </p>
      <p>
-      <input type="radio" name="yes_no" value="no" id="no" required>Nope!!!!</input>
+      <input type="radio" name="yes_no" value="false" id="no">Nope!!!!</input>
      </p>
     </div>
 
  </div>`;
 
 root.insertAdjacentHTML('afterbegin', rootTemplate);
+
+
+// Variables and eventlisteners to retrieve input values
+
+let userEmail = document.getElementById('email')
+let userPassword = document.getElementById('password')
+let signUpBtn = document.getElementById('signUpBtn');
+let loginBtn = document.getElementById('loginBtn');
+let subscription = document.getElementsByName('yes_no');
+
+
+signUpBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	let subscriptionChoice;
+
+	subscription.forEach(radio => {
+		if (radio.checked) {
+			// console.log(radio.value); FYI BOOLEAN VÄRDENA SOM VISAS I SUBSCRIPTION ÄR TYPEOF STRING OCH INTE BOOLEAN 
+			// console.log(typeof radio.value);
+			return subscriptionChoice = radio.value;
+		}
+	});
+
+
+	let newUser = {
+		userEmail: userEmail.value,
+		userPassword: userPassword.value,
+		subscription: subscriptionChoice
+	}
+
+	console.log(newUser);
+
+	// Fetch adressen för POSTEN måste läggas till
+	fetch('/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/ json'
+		},
+		body: JSON.stringify(newUser)
+	})
+
+
+});
